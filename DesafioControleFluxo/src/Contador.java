eaa8ab9874218a0bb4cd38104708a8930faac34e
kauelimaaa <kaue000070@gmail.com>
2023-10-01 20:25:55 -0300

    DesafioContador

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Contador {
+    public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in).useLocale(Locale.US);
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+		
+		try {
+			contar(parametroUm, parametroDois);
+		
+		}catch (ParametrosInvalidosException exception) {
+			System.err.println("O segundo parametro deve ser maior que o primeiro");
+		}
+		
+	}
+
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+
+		if (parametroUm > parametroDois){
+            throw new ParametrosInvalidosException();
+        }
+		
+		int contagem = parametroDois - parametroUm;
+		
+        for (int n = 1; n <= contagem; n++) {
+            System.out.println("Imprimindo o numero " + n);
+        }
+	}
+}
